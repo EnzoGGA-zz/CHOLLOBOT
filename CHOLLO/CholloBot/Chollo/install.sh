b9e2d05a144aa4b39db8d2b84cc6f71b14d512a2
EnzoGGA <66280266+EnzoGGA@users.noreply.github.com>
2021-02-26 00:49:07 -0300

    Update install.sh

--- CHOLLO/CholloBot/Chollo/install.sh
+++ CHOLLO/CholloBot/Chollo/install.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/bash
 
-apt-get update
-apt-get upgrade
-apt-get install nodejs
-apt-get install libwebp
-apt-get install ffmpeg
-apt-get install wget
-apt-get install tesseract
+echo BOT CHOLLO INSTALAÇÃO
+
+apt-get install nodejs -y
+apt-get install libwebp -y
+apt-get install ffmpeg -y
+apt-get install wget -y
+apt-get install tesseract -y 
+pkg install npm -y
+apt-get install npm -y
+apt-get update -y
+apt-get upgrade -y
+clear
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
+npm i -g pm2
+clear
 
 echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
