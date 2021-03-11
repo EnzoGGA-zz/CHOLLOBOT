7414746c79f10211ab19249f588f1e7253f1f6fe
EnzoGGA <66280266+EnzoGGA@users.noreply.github.com>
2021-03-11 00:26:47 -0300

    Update install.sh

--- CHOLLO/CholloBot/Chollo/install.sh
+++ CHOLLO/CholloBot/Chollo/install.sh
@@ -2,14 +2,17 @@
 
 clear
 echo BOT CHOLLO INSTALAÇÃO
-apt install nodejs -y
-apt install termux-api -y
-apt install libwebp -y
-apt install ffmpeg -y
-apt install wget -y
-apt install tesseract -y
-apt update -y
-apt upgrade -y
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+apt install nodejs
+apt install termux-api
+apt install libwebp
+apt install ffmpeg
+apt install wget
+apt install tesseract
+apt update
+apt upgrade
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 echo Instalação do npm em andamento...
 npm install
