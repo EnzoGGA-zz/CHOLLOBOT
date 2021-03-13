5334f18d32a75900842f14274c19316058dd1483
EnzoGGA <66280266+EnzoGGA@users.noreply.github.com>
2021-03-12 23:42:28 -0300

    Update install.sh

--- CHOLLO/CholloBot/Chollo/install.sh
+++ CHOLLO/CholloBot/Chollo/install.sh
@@ -1,20 +1,13 @@
-echo BOT CHOLLO INSTALAÇÃO
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
 apt-get install nodejs
 apt-get install libwebp
 apt-get install ffmpeg
 apt-get install wget
 apt-get install tesseract
-apt-get update
-apt-get upgrade
-pkg install root-repo
-pkg install unstable-repo
-pkg install x11-repo
-clear
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-clear
-echo Instalação do npm em andamento...
 npm install
-npm -g install ytdl
-npm install shelljs
-npm i -g pm2
-echo "[*] Todas as dependencias foram instaladas"
+
+echo "Instalado"
