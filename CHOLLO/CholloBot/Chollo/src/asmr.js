ddb3570d1a925a983cafcd0a6fc3463461609565
EnzoGGA <66280266+EnzoGGA@users.noreply.github.com>
2021-04-11 14:43:56 -0300

    Add files via upload

---
+++ CHOLLO/CholloBot/Chollo/src/asmr.js
@@ -0,0 +1,29 @@
+const asmr = (prefix) => {
+
+	return `Opa, vc gosta de ASMR? Conheça o grupo *Community ASMR 💬*
+
+https://chat.whatsapp.com/EWYBsg9IBr1FozuOjKFqtK
+
+
+Canal dos ASMRTISTAS: 
+
+Vinicius ASMR
+https://youtube.com/c/ViniciusASMR1
+
+Player2 ASMR
+https://youtube.com/c/Player2ASMR
+
+KC ASMR
+https://youtube.com/c/KCASMR_YT
+
+Gabi ASMR
+https://youtube.com/c/GabiASMR
+
+Conheça tb o aplicativo:
+
+https://play.google.com/store/apps/details?id=br.com.app.gpu2577240.gpuc019e29eb4d9644fdf836ac4ecc1576f 
+
+Tmj`
+}
+exports.asmr = asmr
+
